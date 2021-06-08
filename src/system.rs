73396ee3f4933dca31e15cf1d1b2aeb132b42850
lamafab <42901763+lamafab@users.noreply.github.com>
2021-06-08 19:04:50 +0200

    init system module

---
+++ src/system.rs
@@ -0,0 +1,17 @@
+use crate::chain_api::ChainApi;
+use crate::database::Database;
+use crate::{Context, Result};
+
+const ROW_AMOUNT: usize = 25;
+
+pub async fn run_transfer_listener(Contextes: Vec<Context>) -> Result<()> {
+    let chain = ChainApi::new();
+    let db = Database::new("", "").await?;
+
+    let mut page = 1;
+    for Context in &Contextes {
+        let resp = chain.request_transfers(Context, ROW_AMOUNT, page).await?;
+    }
+
+    Ok(())
+}
